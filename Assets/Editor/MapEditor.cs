48f3570094c61aa82adb9e175fed6cac3a420fbe
Vincent <vincentcolano@gmail.com>
2019-02-06 17:52:41 -0600

    editor script

---
+++ Assets/Editor/MapEditor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+
+[CustomEditor (typeof (WorldChunkGenerator))]
+public class MapEditor : Editor
+{
+    public override void OnInspectorGUI() {
+        base.OnInspectorGUI();
+
+        WorldChunkGenerator map = target as WorldChunkGenerator;
+
+        map.GenerateMap();
+    }
+}
--- Assets/Scripts/WorldChunkGenerator.cs
+++ Assets/Scripts/WorldChunkGenerator.cs
@@ -7,17 +7,28 @@ public class WorldChunkGenerator : MonoBehaviour
     public Transform tilePrefab;
     public Vector2 mapSize;
     public Vector2 tileSize;
+    [Range(0,1)]
+    public float outLinePercent;
 
     void Start() {
-        tilePrefab.transform.localScale = new Vector3(tileSize.x, tileSize.y, 1f);
         GenerateMap();
     }
 
     public void GenerateMap() {
+
+        string HolderName = "Generated Map";
+        if (transform.Find(HolderName)) {
+            DestroyImmediate(transform.Find(HolderName).gameObject);
+        }
+        Transform mapHolder = new GameObject(HolderName).transform;
+        mapHolder.parent = transform;
+
         for (int x = 0; x < mapSize.x; x++) {
             for (int y = 0; y < mapSize.y; y++) {
-                Vector3 tilePosition = new Vector3(-mapSize.x/2 + (0.5f*tileSize.x) + x*tileSize.x, 0, -mapSize.y/2 + (0.5f*tileSize.y) + y*tileSize.y);
-                Transform newTile = Instantiate(tilePrefab, tilePosition, Quaternion.Euler(Vector3.right * 90), transform) as Transform;
+                Vector3 tilePosition = new Vector3((-mapSize.x/2 + 0.5f + x)*tileSize.x, 0,(-mapSize.y/2 + 0.5f + y)*tileSize.y);
+                Transform newTile = Instantiate(tilePrefab, tilePosition, Quaternion.Euler(Vector3.right * 90), mapHolder.transform) as Transform;
+                newTile.localScale = new Vector3(tileSize.x * (1 - outLinePercent), tileSize.y * (1 - outLinePercent), 1);
+                
             }
         }
     }
